7ccb08bed52f8ad23ed9e11772774b52037d4d87
Tareq-Bilal <tareqbilal44@gmail.com>
2025-07-06 00:00:44 +0300

    Initial Commit

--- src/MyApp.Application/obj/Debug/net8.0/MyApp.Application.AssemblyInfo.cs
+++ src/MyApp.Application/obj/Debug/net8.0/MyApp.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f32f425a6ac97b26d0df220a8af4b9101e94d61")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
